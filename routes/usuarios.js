b07835003b538af9e9b6625958b74ba1729337ee
Ecarcomo <emmanuelcarcomo@gmail.com>
2025-08-18 10:40:47 -0300

    auth change

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,7 +5,7 @@ const { crearUsuario, obtenerTodosLosUsuarios,obtenerUsuariosDescendientes,obten
 
 router.post("/", auth, crearUsuario);
 router.get("/delegados", auth, obtenerUsuariosDescendientes);
-router.get("/",  obtenerTodosLosUsuarios);
+router.get("/", auth, obtenerTodosLosUsuarios);
 
 router.get("/delegados/:delegado_a_id", auth, obtenerUsuariosDescendientesHermanos);
 router.get("/:id", auth, obtenerUsuario);
